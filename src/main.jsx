21b86ade202c3e8db9f73845442ad434a6521229
Jayaat Kakkar <jayaat.kakkar@mobilefirstapplications.com>
2026-05-21 13:55:29 +0530

    Final Push

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
 
+
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || ''
 
 ReactDOM.createRoot(document.getElementById('root')).render(
